ed038bf37c2e034ff2c7aa3941574d675da4f25a
nathanmc <nathanmc@amazon.com>
2020-06-17 11:35:24 -0400

    Remove unneeded Copyright date.

--- cfn-guard/tests/functional.rs
+++ cfn-guard/tests/functional.rs
@@ -1,4 +1,4 @@
-// © 2020 Amazon Web Services, Inc. or its affiliates. All Rights Reserved. This AWS Content is provided subject to the terms of the AWS Customer Agreement available at http://aws.amazon.com/agreement or other written agreement between Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
+// © Amazon Web Services, Inc. or its affiliates. All Rights Reserved. This AWS Content is provided subject to the terms of the AWS Customer Agreement available at http://aws.amazon.com/agreement or other written agreement between Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 
 // Tests
 use std::env;
