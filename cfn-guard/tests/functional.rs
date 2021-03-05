a534ab5848ae57b5503e33f827b5ce784dfe8a9b
omkhegde <69654230+omkhegde@users.noreply.github.com>
2021-03-05 11:46:23 -0800

    Update lambda wrapper test expected output after latest changes (#16)

--- cfn-guard/tests/functional.rs
+++ cfn-guard/tests/functional.rs
@@ -5,99 +5,100 @@ use cfn_guard;
 
 mod tests {
     use super::*;
-     #[test]
-        fn test_run_check() {
-            let mut data = String::from(
-                r#"
-                    {
-                        "Resources": {
-                            "VPC" : {
-                                "Type" : "AWS::ApiGateway::Method",
-                                "Properties" : {
-                                    "AuthorizationType" : "10.0.0.0/24"
-                                }
+    #[test]
+    fn test_run_check() {
+        let mut data = String::from(
+            r#"
+                {
+                    "Resources": {
+                        "VPC" : {
+                            "Type" : "AWS::ApiGateway::Method",
+                            "Properties" : {
+                                "AuthorizationType" : "10.0.0.0/24"
                             }
                         }
                     }
-                "#,
-            );
-            let rule = "AWS::ApiGateway::Method { Properties.AuthorizationType == \"NONE\"}";
-            let mut expected = String::from(
-                r#"
-                    [
+                }
+            "#,
+        );
+        let rule = "AWS::ApiGateway::Method { Properties.AuthorizationType == \"NONE\"}";
+        let mut expected = String::from(
+            r#"
+            [
+              {
+                "eval_type": "Rule",
+                "context": "default",
+                "from": null,
+                "to": null,
+                "status": "SKIP",
+                "children": [
+                  {
+                    "eval_type": "Type",
+                    "context": "AWS::ApiGateway::Method",
+                    "from": null,
+                    "to": null,
+                    "status": "SKIP",
+                    "children": [
                       {
-                        "eval_type": "Rule",
-                        "context": "default",
+                        "eval_type": "Filter",
+                        "context": "Path=/Resources/VPC,Type=MapElement",
                         "from": null,
                         "to": null,
-                        "status": "FAIL",
+                        "status": "PASS",
                         "children": [
                           {
-                            "eval_type": "Type",
-                            "context": "AWS::ApiGateway::Method",
+                            "eval_type": "Clause",
+                            "context": "GuardAccessClause[ check = Type  EQUALS String(\"AWS::ApiGateway::Method\"), loc = Location[file=#1@14] ]",
                             "from": null,
                             "to": null,
+                            "status": "PASS",
+                            "children": []
+                          }
+                        ]
+                      },
+                      {
+                        "eval_type": "Type",
+                        "context": "AWS::ApiGateway::Method#0(/Resources/VPC)",
+                        "from": null,
+                        "to": null,
+                        "status": "SKIP",
+                        "children": [
+                          {
+                            "eval_type": "Clause",
+                            "context": "GuardAccessClause[ check = Properties.AuthorizationType  EQUALS String(\"NONE\"), loc = Location[file=lambda#1@27] ]",
+                            "from": {
+                              "String": [
+                                "/Resources/VPC/Properties/AuthorizationType",
+                                "10.0.0.0/24"
+                              ]
+                            },
+                            "to": {
+                              "String": [
+                                "lambda/1/27/Clause/",
+                                "NONE"
+                              ]
+                            },
                             "status": "FAIL",
-                            "children": [
-                              {
-                                "eval_type": "Filter",
-                                "context": "Path=/Resources/VPC,Type=MapElement",
-                                "from": null,
-                                "to": null,
-                                "status": "PASS",
-                                "children": [
-                                  {
-                                    "eval_type": "Clause",
-                                    "context": "GuardAccessClause[ check = Type  EQUALS String(\"AWS::ApiGateway::Method\"), loc = Location[file=#1@14] ]",
-                                    "from": null,
-                                    "to": null,
-                                    "status": "PASS",
-                                    "children": []
-                                  }
-                                ]
-                              },
-                              {
-                                "eval_type": "Type",
-                                "context": "AWS::ApiGateway::Method#0(/Resources/VPC)",
-                                "from": null,
-                                "to": null,
-                                "status": "FAIL",
-                                "children": [
-                                  {
-                                    "eval_type": "Clause",
-                                    "context": "GuardAccessClause[ check = Properties.AuthorizationType  EQUALS String(\"NONE\"), loc = Location[file=lambda#1@27] ]",
-                                    "from": {
-                                      "String": [
-                                        "/Resources/VPC/Properties/AuthorizationType",
-                                        "10.0.0.0/24"
-                                      ]
-                                    },
-                                    "to": {
-                                      "String": [
-                                        "lambda/1/27/Clause/",
-                                        "NONE"
-                                      ]
-                                    },
-                                    "status": "FAIL",
-                                    "children": []
-                                  }
-                                ]
-                              }
-                            ]
+                            "children": []
                           }
                         ]
                       }
                     ]
-                "#,
-                );
+                  }
+                ]
+              }
+            ]
+            "#,
+            );
 
-            // Remove white spaces from expected and calculated result for easy comparison.
-            expected.retain(|c| !c.is_whitespace());
+        // Remove white spaces from expected and calculated result for easy comparison.
+        expected.retain(|c| !c.is_whitespace());
 
-            let mut serialized =   cfn_guard::run_checks(&data, &rule).unwrap();
-            serialized.retain(|c| !c.is_whitespace());
+        let mut serialized =   cfn_guard::run_checks(&data, &rule).unwrap();
+        println!("{}", serialized);
+        serialized.retain(|c| !c.is_whitespace());
 
-            assert_eq!(expected, serialized);
-        }
+        assert_eq!(expected, serialized);
+    }
 
 }
