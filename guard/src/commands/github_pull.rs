1df464d4a1e6ccefe875269aabab17209b19485a
Minh Phuong <phuongmi@amazon.com>
2022-06-28 10:43:13 -0700

    add version functions and test

--- guard/src/commands/github_pull.rs
+++ guard/src/commands/github_pull.rs
@@ -1,4 +1,5 @@
 use config::Config;
+use semver::{BuildMetadata, Prerelease, Version, VersionReq};
 
 pub struct GitHubSource {
     pub user: String,
@@ -23,6 +24,55 @@ impl GitHubSource {
         format!("GitHubSource user({}) repo ({}) file_name({}) access_token({}) version_needed({})",
                 &self.user, &self.repo, &self.file_name, &self.access_token, &self.version_needed)
     }
+
+    fn read_config(&self) ->GitHubSource {
+        let settings = Config::builder()
+            .add_source(config::File::with_name("src/config"))
+            .build()
+            .unwrap();
+
+        let args = settings.try_deserialize::<HashMap<String, String>>().unwrap();
+        let version_needed = args.get("version_needed").unwrap();
+        let experimental = args.get("experimental").unwrap();
+        return GitHubSource::new(user,repo,file_name,access_token,version_needed,experimental);
+    }
+
+    async fn get_version(&self) -> Vec<String> {
+        let octo = octocrab::OctocrabBuilder::new()
+            .personal_token(self.access_token)
+            .build()
+            .unwrap();
+
+        let page = octo
+            .repos(self.owner, self.repo_name)
+            .releases()
+            .list()
+            .per_page(100)
+            .page(1u32)
+            .send()
+            .await?;
+        let mut versions:Vec<String> = Vec::new();
+        for item in page.take_items(){
+            println!("{}",item.tag_name);
+            let tag_cleaned = item.tag_name.replace("v", "");
+            // TODO: remove the "pre", filter unstable if experimental is false
+            versions.push(tag_cleaned)
+        };
+        return versions;
+    }
+
+    fn get_satisfied(&self) -> String {
+        let req = Version::parse(&self.version_needed).unwrap();
+        let mut output;
+        // dependency resolution
+        for version in &versions.rev() {
+            if Version::parse(&version).unwrap().matches(&req){
+                output = version;
+                break
+            }
+        }
+        return output;
+    }
 }
 impl Pull for GitHubSource {
     fn pull(&self) -> octocrab::Result<ContentItems> {
@@ -38,18 +88,6 @@ impl Pull for GitHubSource {
             .send()
             .await?;
         Ok(repo)
-    }
-
-    fn read_config(&self) ->GitHubSource {
-        let settings = Config::builder()
-            .add_source(config::File::with_name("src/config"))
-            .build()
-            .unwrap();
-
-        let args = settings.try_deserialize::<HashMap<String, String>>().unwrap();
-        let version_needed = args.get("version_needed").unwrap();
-        let experimental = args.get("experimental").unwrap();
-        return GitHubSource::new(user,repo,file_name,access_token,version_needed,experimental);
     }
 }
 
--- guard/src/commands/github_pull_test.rs
+++ guard/src/commands/github_pull_test.rs
@@ -6,3 +6,9 @@ fn test_create() {
    let gh = GitHubSource::new("a","b","c","d","e","f");
     assert_eq!(gh.user.to_string,"a");
 }
+
+fn test_get_satisfied(){
+    let versions = ["0.1.0", "0.1.1", "0.1.2"].iter().map(|&s| s.into()).collect();
+    let gh = GitHubSource::new("a","b","c","d","e","0.1.0");
+    assert_eq!(gh.get_satisfied(versions), "0.1.2");
+}
