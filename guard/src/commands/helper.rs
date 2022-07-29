6b83c521b7840fa82534422cb98ea90ce959708e
Akshay Rane <raneaks@amazon.com>
2022-07-29 14:53:17 -0700

    Update lambda handler to emit the new evaluation engine output

--- guard/src/commands/helper.rs
+++ guard/src/commands/helper.rs
@@ -8,7 +8,11 @@ use crate::commands::tracker::StackTracker;
 use crate::commands::validate::{ConsoleReporter, OutputFormatType, Reporter};
 use crate::rules::{Evaluate, Result};
 use std::convert::TryFrom;
+use std::io::BufWriter;
 use crate::commands::validate::generic_summary::GenericSummary;
+use crate::rules::eval::eval_rules_file;
+use crate::rules::eval_context::root_scope;
+use crate::rules::path_value::traversal::Traversal;
 
 pub fn validate_and_return_json(
     data: &str,
@@ -30,17 +34,37 @@ pub fn validate_and_return_json(
         Ok(rules) => {
             match input_data {
                 Ok(root) => {
-                    let root_context = RootScope::new(&rules, &root);
-                    let stacker = StackTracker::new(&root_context);
                     let data_file_name: &str = "lambda-payload";
                     let rules_file_name: &str = "lambda-run";
-                    let renderer = &GenericSummary::new() as &dyn Reporter;
-                    let renderers = vec![renderer];
-                    let reporter = ConsoleReporter::new(stacker, &renderers, &rules_file_name, &data_file_name, verbose, true, false);
-                    rules.evaluate(&root, &reporter)?;
-                    let json_result = reporter.get_result_json(
-                        &root, OutputFormatType::JSON)?;
-                    return Ok(json_result);
+
+                    let mut write_output = BufWriter::new(Vec::new());
+
+                    let traversal = Traversal::from(&root);
+                    let mut root_scope = root_scope(&rules, &root)?;
+                    let status = eval_rules_file(&rules, &mut root_scope)?;
+                    let root_record = root_scope.reset_recorder().extract();
+
+                    if verbose {
+                        return Ok(serde_json::to_string_pretty(&root_record)?);
+                    }
+
+                    let reporter = &GenericSummary::new() as &dyn Reporter;
+
+                    reporter.report_eval(
+                        &mut write_output,
+                        status,
+                        &root_record,
+                        rules_file_name,
+                        data_file_name,
+                        data,
+                        &traversal,
+                        OutputFormatType::JSON
+                    )?;
+
+                    match String::from_utf8(write_output.buffer().to_vec()) {
+                        Ok(val) => return Ok(val),
+                        Err(e) => return Err(Error::new(ErrorKind::ParseError(e.to_string()))),
+                    }
                 }
                 Err(e) => return Err(e),
             }
