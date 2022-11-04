e64fa55f704f8978180c8bfe34c741c8ad362967
Akshay Rane <aks.rane@gmail.com>
2022-11-04 12:26:00 -0700

    Revert "Bumping up clap-rs (#294)" (#299)
    This reverts commit 5481fa7265eab3dfdbb932df3fee4936dd4a4497.
    
    Co-authored-by: Akshay Rane <raneaks@amazon.com>

--- guard/src/command.rs
+++ guard/src/command.rs
@@ -1,9 +1,9 @@
-use clap::ArgMatches;
+use clap::{App, ArgMatches};
 
 use crate::rules::errors::Error;
 
 pub trait Command {
     fn name(&self) -> &'static str;
-    fn command(&self) -> clap::Command;
+    fn command(&self) -> App<'static, 'static>;
     fn execute(&self, args: &ArgMatches) -> Result<i32, Error>;
 }
--- guard/tests/utils.rs
+++ guard/tests/utils.rs
@@ -1,14 +1,14 @@
 // Copyright Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::PathBuf;
-
+use cfn_guard::commands::validate::Validate;
+use std::collections::HashMap;
+use clap::App;
 use cfn_guard::command::Command;
 use cfn_guard::commands::{DATA, RULES};
-use cfn_guard::commands::validate::Validate;
 
 pub fn get_data_option() -> String {
     format!("-{}", DATA.1)
@@ -36,10 +36,11 @@ pub fn get_full_path_for_resource_file(path: &str) -> String {
 
 pub fn cfn_guard_test_command(args: Vec<&str>) -> i32 {
     let TEST_APP_NAME = "cfn-guard-test";
-    let mut app = clap::Command::new(TEST_APP_NAME);
+    let mut app =
+        App::new(TEST_APP_NAME);
     let mut command_options = Vec::new();
     command_options.push(TEST_APP_NAME);
-    command_options.append(args.clone().as_mut());
+    command_options.append( args.clone().as_mut());
 
     let mut commands: Vec<Box<dyn Command>> = Vec::with_capacity(2);
     commands.push(Box::new(Validate::new()));
@@ -51,7 +52,7 @@ pub fn cfn_guard_test_command(args: Vec<&str>) -> i32 {
         |mut map, entry| {
             map.insert(entry.0, entry.1.as_ref());
             map
-        },
+        }
     );
 
     for each in &commands {
@@ -61,19 +62,26 @@ pub fn cfn_guard_test_command(args: Vec<&str>) -> i32 {
     let app = app.get_matches_from(command_options);
 
     match app.subcommand() {
-        Some((name, value)) => {
+        (name, Some(value)) => {
             if let Some(command) = mappings.get(name) {
                 match (*command).execute(value) {
                     Err(e) => {
                         println!("Error occurred {}", e);
-                        -1
+                        return -1;
+                    },
+                    Ok(code) => {
+                        return code;
                     }
-                    Ok(code) => code
                 }
-            } else {
-                -2
             }
+            else {
+                return -2;
+            }
+        },
+
+        (_, None) => {
+            return -3;
         }
-        None => -3
     }
+
 }
