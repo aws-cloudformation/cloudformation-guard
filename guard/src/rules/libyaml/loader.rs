202fbe4a4da76dd48390f015f733e19475211f9f
Josh Fried <112121129+joshfried-aws@users.noreply.github.com>
2023-02-08 16:09:35 -0500

    clippy lints

--- guard/src/rules/libyaml/loader.rs
+++ guard/src/rules/libyaml/loader.rs
@@ -32,30 +32,29 @@ impl Loader {
         let mut parser = Parser::new(Cow::Borrowed(content.as_bytes()));
 
         loop {
-            match parser.next()? {
-                (event, location) => {
-                    match event {
-                        Event::StreamStart | Event::StreamEnd | Event::DocumentStart => {}
-                        Event::DocumentEnd => {
-                            self.documents.push(self.stack.pop().unwrap());
-                            self.stack.clear();
-                            self.last_container_index.clear();
-                            return Ok(self.documents.pop().unwrap());
-                        }
-                        Event::MappingStart(..) => self.handle_mapping_start(location),
-                        Event::MappingEnd => self.handle_mapping_end(),
-                        Event::SequenceStart(sequence_start) => {
-                            self.handle_sequence_start(sequence_start, location)
-                        }
-                        Event::SequenceEnd => self.handle_sequence_end(),
-                        Event::Scalar(scalar) => self.handle_scalar_event(scalar, location),
-                        Event::Alias(_) => {
-                            return Err(Error::ParseError(String::from(
-                                "Guard does not currently support aliases",
-                            )))
-                        }
-                    };
-                }
+            let (event, location) = parser.next()?;
+            {
+                match event {
+                    Event::StreamStart | Event::StreamEnd | Event::DocumentStart => {}
+                    Event::DocumentEnd => {
+                        self.documents.push(self.stack.pop().unwrap());
+                        self.stack.clear();
+                        self.last_container_index.clear();
+                        return Ok(self.documents.pop().unwrap());
+                    }
+                    Event::MappingStart(..) => self.handle_mapping_start(location),
+                    Event::MappingEnd => self.handle_mapping_end(),
+                    Event::SequenceStart(sequence_start) => {
+                        self.handle_sequence_start(sequence_start, location)
+                    }
+                    Event::SequenceEnd => self.handle_sequence_end(),
+                    Event::Scalar(scalar) => self.handle_scalar_event(scalar, location),
+                    Event::Alias(_) => {
+                        return Err(Error::ParseError(String::from(
+                            "Guard does not currently support aliases",
+                        )))
+                    }
+                };
             };
         }
     }
