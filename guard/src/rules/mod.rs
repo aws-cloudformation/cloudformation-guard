4baf9558e66ce23e96d1db80676251d3b4cd6e28
Josh Fried <112121129+joshfried-aws@users.noreply.github.com>
2023-02-13 11:28:10 -0500

    improving handling for tagged values for test command

--- guard/src/rules/mod.rs
+++ guard/src/rules/mod.rs
@@ -17,11 +17,72 @@ use crate::rules::exprs::{ParameterizedRule, QueryPart};
 use crate::rules::path_value::PathAwareValue;
 use crate::rules::values::CmpOperator;
 use colored::*;
+use lazy_static::lazy_static;
 use nom::lib::std::convert::TryFrom;
 use serde::Serialize;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Formatter;
 
 pub(crate) type Result<R> = std::result::Result<R, Error>;
+
+lazy_static! {
+    pub static ref SHORT_FORM_TO_LONG_MAPPING: HashMap<&'static str, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert("Ref", "Ref");
+        m.insert("GetAtt", "Fn::GetAtt");
+        m.insert("Base64", "Fn::Base64");
+        m.insert("Sub", "Fn::Sub");
+        m.insert("GetAZs", "Fn::GetAZs");
+        m.insert("ImportValue", "Fn::ImportValue");
+        m.insert("Condition", "Condition");
+        m.insert("RefAll", "Fn::RefAll");
+        m.insert("Select", "Fn::Select");
+        m.insert("Split", "Fn::Split");
+        m.insert("Join", "Fn::Join");
+        m.insert("FindInMap", "Fn::FindInMap");
+        m.insert("And", "Fn::And");
+        m.insert("Equals", "Fn::Equals");
+        m.insert("Contains", "Fn::Contains");
+        m.insert("EachMemberIn", "Fn::EachMemberIn");
+        m.insert("EachMemberEquals", "Fn::EachMemberEquals");
+        m.insert("ValueOf", "Fn::ValueOf");
+        m.insert("If", "Fn::If");
+        m.insert("Not", "Fn::Not");
+        m.insert("Or", "Fn::Or");
+        m
+    };
+    static ref SINGLE_VALUE_FUNC_REF: HashSet<&'static str> = {
+        let mut set = HashSet::new();
+        set.insert("Ref");
+        set.insert("Base64");
+        set.insert("Sub");
+        set.insert("GetAZs");
+        set.insert("ImportValue");
+        set.insert("GetAtt");
+        set.insert("Condition");
+        set.insert("RefAll");
+        set
+    };
+    static ref SEQUENCE_VALUE_FUNC_REF: HashSet<&'static str> = {
+        let mut set = HashSet::new();
+        set.insert("GetAtt");
+        set.insert("Sub");
+        set.insert("Select");
+        set.insert("Split");
+        set.insert("Join");
+        set.insert("FindInMap");
+        set.insert("And");
+        set.insert("Equals");
+        set.insert("Contains");
+        set.insert("EachMemberIn");
+        set.insert("EachMemberEquals");
+        set.insert("ValueOf");
+        set.insert("If");
+        set.insert("Not");
+        set.insert("Or");
+        set
+    };
+}
 
 #[derive(Debug, Clone, PartialEq, Copy, Serialize)]
 pub(crate) enum Status {
@@ -343,4 +404,11 @@ pub(crate) trait Evaluate {
         context: &'s PathAwareValue,
         var_resolver: &'s dyn EvaluationContext,
     ) -> Result<Status>;
+}
+
+pub fn short_form_to_long(fn_ref: &str) -> &'static str {
+    match SHORT_FORM_TO_LONG_MAPPING.get(fn_ref) {
+        Some(fn_ref) => fn_ref,
+        _ => unreachable!(),
+    }
 }
