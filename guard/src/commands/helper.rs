4cbd9bcc36f726143ac66667801d0ff7747954ac
Josh Fried <112121129+joshfried-aws@users.noreply.github.com>
2023-06-12 16:37:20 -0400

    helper.rs clippy lints

--- guard/src/commands/helper.rs
+++ guard/src/commands/helper.rs
@@ -13,20 +13,22 @@ use std::convert::TryFrom;
 use std::io::BufWriter;
 use std::rc::Rc;
 
+#[allow(dead_code)]
 pub struct ValidateInput<'a> {
     pub content: &'a str,
     pub file_name: &'a str,
 }
 
+#[allow(dead_code)]
 pub fn validate_and_return_json(
     data: ValidateInput,
     rules: ValidateInput,
     verbose: bool,
 ) -> Result<String> {
-    let path_value = match serde_json::from_str::<serde_json::Value>(&data.content) {
+    let path_value = match serde_json::from_str::<serde_json::Value>(data.content) {
         Ok(value) => PathAwareValue::try_from(value),
         Err(_) => {
-            let value = serde_yaml::from_str::<serde_yaml::Value>(&data.content)?;
+            let value = serde_yaml::from_str::<serde_yaml::Value>(data.content)?;
             PathAwareValue::try_from(value)
         }
     }
@@ -43,7 +45,7 @@ pub fn validate_and_return_json(
         name: data.file_name.to_owned(),
     };
 
-    let span = crate::rules::parser::Span::new_extra(&rules.content, rules.file_name);
+    let span = crate::rules::parser::Span::new_extra(rules.content, rules.file_name);
 
     let rules_file_name = rules.file_name;
     return match crate::rules::parser::rules_file(span) {
