13be3d79da68bc67078da17d11f1cf6fd7493445
Josh Fried <joshfri@amazon.com>
2024-01-17 12:22:29 -0500

    updating error message for quick_xml errors

--- guard/src/rules/errors.rs
+++ guard/src/rules/errors.rs
@@ -45,7 +45,7 @@ pub enum Error {
     Errors(#[from] Errors),
     #[error("{0}")]
     IllegalArguments(String),
-    #[error("{0}")]
+    #[error("Error occured while attempting to write junit report")]
     XMLError(#[from] quick_xml::Error),
     #[error("{0}")]
     InternalError(InternalError),
