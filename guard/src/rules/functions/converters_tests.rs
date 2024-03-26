65d1440e91fd90c372441466cf825ce4da723956
Josh Fried <joshfri@amazon.com>
2024-03-26 10:00:08 -0400

    cleanup

--- guard/src/rules/functions/converters_tests.rs
+++ guard/src/rules/functions/converters_tests.rs
@@ -126,7 +126,7 @@ fn test_parse_float() -> crate::rules::Result<()> {
         Type: AWS::EC2::SecurityGroup
         Properties:
           SecurityGroupIngress:
-            String: "2234"
+            String: "2.0"
             Int: 1
             Float: 1.0
             BadValue: "123 not a real number"
