eee428594282a1f0cf12f22c61dd434c39c1f855
Dan Vassallo <danielvassallo87@gmail.com>
2025-02-18 10:50:47 -0500

    chore(action): run conversion on eslint config so lint passes

--- action/dist/index.js
+++ action/dist/index.js
@@ -33239,7 +33239,6 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
  * The entrypoint for the action.
  */
 const main_1 = __nccwpck_require__(1730);
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 (0, main_1.run)();
 
 })();
--- action/src/index.ts
+++ action/src/index.ts
@@ -3,5 +3,4 @@
  */
 import { run } from './main';
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 run();
